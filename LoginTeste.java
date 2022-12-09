af6226cbca6653e3aa24bae575e2cdc23adc3c71
Larissa Flausino <110359121+LarissaFlausino@users.noreply.github.com>
2022-12-09 16:13:07 -0300

    Add files via upload

---
+++ LoginTeste.java
@@ -0,0 +1,69 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginTeste {
+
+	@Given("que acesse a tela de login")
+	public void queAcesseATelaDeLogin() {
+
+	}
+
+	@Given("username")
+	public void username() {
+
+	}
+
+	@Given("password")
+	public void password() {
+
+	}
+
+	@When("clicar em Login")
+	public void clicarEmLogin() {
+
+	}
+
+	@Then("usuario logado com sucesso")
+	public void usuarioLogadoComSucesso() {
+
+	}
+
+	@Given("nao informe nenhum dado")
+	public void naoInformeNenhumDado() {
+
+	}
+
+	@When("clicar em login")
+	public void clicarEmLogin1() {
+
+	}
+
+	@Then("valido mensagem de erro de dados em branco")
+	public void validoMensagemDeErroDeDadosEmBranco() {
+
+	}
+
+	@Given("informe dados invalidos")
+	public void informeDadosInvalidos() {
+
+	}
+
+	@Then("valido mensagem de erro de dados invalidos")
+	public void validoMensagemDeErroDeDadosInvalidos() {
+
+	}
+
+	@Given("usuario bloqueado")
+	public void usuarioBloqueado() {
+
+	}
+
+	@Then("valido mensagem de erro de usuario bloqueado")
+	public void validoMensagemDeErroDeUsuarioBloqueado() {
+
+	}
+
+}
---
+++ MassaTestes.java
@@ -0,0 +1,11 @@
+package dados;
+
+public class MassaTestes {
+
+	public String standard_user = "standard_user";
+	public String locked_out_user = "locked_out_user";
+	public String problem_user = "problem_user";
+	public String performance_glitch_user = "performance_glitch_user";
+	public String secret_sauce = "secret_sauce";
+
+}
